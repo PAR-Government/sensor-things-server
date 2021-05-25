ef67193fa9ef8e1eed52118eb72fe8e8cd825ca8
Gregory Hasseler <gregory_hasseler@partech.com>
2021-05-25 16:37:38 -0500

    Added test objects

---
+++ SensorThings.Server.Test/TestObjects/TestRepoFactory.cs
@@ -0,0 +1,32 @@
+﻿using SensorThings.Entities;
+using SensorThings.Server.Repositories;
+using System;
+
+namespace SensorThings.Server.Test.TestObjects
+{
+
+    public class TestRepoFactory : IRepositoryFactory
+    {
+        public IThingsRepository ThingsRepository { get; set; }
+
+        public IRepositoryUnitOfWork CreateUnitOfWork()
+        {
+            return new TestUOW() { ThingsRepository = ThingsRepository };
+        }
+
+        public class TestUOW : IRepositoryUnitOfWork
+        {
+            public IThingsRepository ThingsRepository { get; set; }
+
+            public IRepository<Location> LocationsRepository => throw new NotImplementedException();
+
+            public void Commit()
+            {
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
