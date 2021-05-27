56887b1bba2458f8efe6d756601a42bebda4b83a
Gregory Hasseler <gregory_hasseler@partech.com>
2021-05-26 21:57:08 -0500

    Fixed DateTime format issue

--- SensorThings.Entities/JsonConverters/JsonOGCTimeConverter.cs
+++ SensorThings.Entities/JsonConverters/JsonOGCTimeConverter.cs
@@ -7,22 +7,27 @@ namespace SensorThings.Entities.JsonConverters
 {
     public class JsonOGCTimeConverter : JsonConverter<OGCTime>
     {
+        private readonly string dateFormat = "yyyy-MM-ddTHH:mm:ssZ";
+
         public override OGCTime ReadJson(JsonReader reader, Type objectType, OGCTime existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
             string s = (string)reader.Value;
 
             // Split into date halves
             var dates = s.Split('/');
-            var start = DateTime.Parse(dates[0]);
-            var stop = DateTime.Parse(dates[1]);
+
+            var start = DateTime.ParseExact(dates[0], dateFormat,
+                System.Globalization.CultureInfo.InvariantCulture);
+            var stop = DateTime.ParseExact(dates[1], dateFormat,
+                System.Globalization.CultureInfo.InvariantCulture);
 
             return new OGCTime(start, stop);
         }
 
         public override void WriteJson(JsonWriter writer, OGCTime value, JsonSerializer serializer)
         {
-            var start = value.Start.ToString("yyyyMMddTHH:mm:ssZ");
-            var stop = value.Stop.ToString("yyyyMMddTHH:mm:ssZ");
+            var start = value.Start.ToString(dateFormat);
+            var stop = value.Stop.ToString(dateFormat);
 
             writer.WriteValue($"{start}/{stop}");
         }
