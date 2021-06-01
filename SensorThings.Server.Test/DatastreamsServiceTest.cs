e4677d880f5579f5d464c7f5706b9187193f881f
Gregory Hasseler <gregory_hasseler@partech.com>
2021-05-31 22:00:24 -0500

    Fill in missing unit tests

--- SensorThings.Server.Test/DatastreamsServiceTest.cs
+++ SensorThings.Server.Test/DatastreamsServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using Newtonsoft.Json.Linq;
 using SensorThings.Entities;
 using SensorThings.Server.Repositories;
 using SensorThings.Server.Services;
@@ -24,6 +25,86 @@ namespace SensorThings.Server.Test
             var createdDatastream = await service.AddDatastream(datastream);
 
             datastreamRepoMock.Verify(m => m.AddAsync(createdDatastream));
+        }
+
+        [Fact]
+        public async Task Test_GetDatastreamById()
+        {
+            int id = 42;
+            Datastream datastream = new Datastream { ID = id, Name = "Test Datastream" };
+            Mock<IRepository<Datastream>> datastreamRepoMock = new Mock<IRepository<Datastream>>();
+            datastreamRepoMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync(datastream);
+            var repoFactory = new TestRepoFactory { DatastreamsRepository = datastreamRepoMock.Object };
+            var service = new DatastreamsService(repoFactory);
+
+            var retrievedDatastream = await service.GetDatastreamById(id);
+
+            Assert.NotNull(retrievedDatastream);
+            Assert.Equal(datastream, retrievedDatastream);
+        }
+
+        [Fact]
+        public async Task Test_GetDatastreams()
+        {
+            var datastreamCollection = new List<Datastream> 
+            { 
+                new Datastream { ID = 1, Name = "Test Datastream" } , 
+                new Datastream { ID = 2, Name = "Test Datastream" } 
+            };
+            Mock<IRepository<Datastream>> datastreamRepoMock = new Mock<IRepository<Datastream>>();
+            datastreamRepoMock.Setup(m => m.GetAllAsync()).ReturnsAsync(datastreamCollection);
+            var repoFactory = new TestRepoFactory { DatastreamsRepository = datastreamRepoMock.Object };
+            var service = new DatastreamsService(repoFactory);
+
+            var datastreams = await service.GetDatastreams();
+
+            datastreamRepoMock.Verify(m => m.GetAllAsync());
+            Assert.NotEmpty(datastreams);
+        }
+
+        [Fact]
+        public async Task Test_RemoveDatastream()
+        {
+            int id = 42;
+            Mock<IRepository<Datastream>> datastreamRepoMock = new Mock<IRepository<Datastream>>();
+            var repoFactory = new TestRepoFactory { DatastreamsRepository = datastreamRepoMock.Object };
+            var service = new DatastreamsService(repoFactory);
+
+            await service.RemoveDatastream(id);
+
+            datastreamRepoMock.Verify(m => m.Remove(id));
+        }
+
+        [Fact]
+        public async Task Test_Update_EmptyFields()
+        {
+            var updates = JObject.Parse("{}");
+            int id = 42;
+            Datastream datastream = new Datastream { ID = id, Name = "Test Datastream" };
+            Mock<IRepository<Datastream>> datastreamRepoMock = new Mock<IRepository<Datastream>>();
+            datastreamRepoMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync(datastream);
+            var repoFactory = new TestRepoFactory { DatastreamsRepository = datastreamRepoMock.Object };
+            var service = new DatastreamsService(repoFactory);
+
+            var updatedDatastream = await service.UpdateDatastream(updates, id);
+
+            Assert.Equal("Test Datastream", updatedDatastream.Name);
+        }
+
+        [Fact]
+        public async Task Test_Update_NonEmptyFields()
+        {
+            var updates = JObject.Parse("{\"Name\": \"FOO BAR\"}");
+            int id = 42;
+            Datastream datastream = new Datastream { ID = id, Name = "Test Datastream" };
+            Mock<IRepository<Datastream>> datastreamRepoMock = new Mock<IRepository<Datastream>>();
+            datastreamRepoMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync(datastream);
+            var repoFactory = new TestRepoFactory { DatastreamsRepository = datastreamRepoMock.Object };
+            var service = new DatastreamsService(repoFactory);
+
+            var updatedDatastream = await service.UpdateDatastream(updates, id);
+
+            Assert.Equal("FOO BAR", updatedDatastream.Name);
         }
     }
 }
