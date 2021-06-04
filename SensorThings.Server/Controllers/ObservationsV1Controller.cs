f68b3970f58eff2ef7acbc90fe9f5b6fb9c34450
Gregory Hasseler <gregory_hasseler@partech.com>
2021-06-04 15:58:49 -0500

    Filled in controller and stubbed out service for Observation

--- SensorThings.Server/Controllers/ObservationsV1Controller.cs
+++ SensorThings.Server/Controllers/ObservationsV1Controller.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using EmbedIO;
 using EmbedIO.Routing;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SensorThings.Entities;
 using SensorThings.Server.Repositories;
+using SensorThings.Server.Services;
 
 namespace SensorThings.Server.Controllers
 {
@@ -13,18 +17,63 @@ namespace SensorThings.Server.Controllers
         public ObservationsV1Controller(IRepositoryFactory repoFactory) : base(repoFactory) { }
 
         [Route(HttpVerbs.Post, "/Observations")]
-        public async Task<string> CreateObservation()
+        public async Task<string> CreateObservationAsync()
         {
             var data = await HttpContext.GetRequestBodyAsStringAsync();
             var observation = JsonConvert.DeserializeObject<Observation>(data);
 
-            return $"name: {observation.PhenomenonTime}";
+            var service = new ObservationsService(RepoFactory);
+            observation = await service.AddObservation(observation);
+
+            observation.BaseUrl = GetBaseUrl();
+
+            Response.StatusCode = (int)HttpStatusCode.Created;
+
+            return JsonConvert.SerializeObject(observation);
         }
 
         [Route(HttpVerbs.Get, "/Observations({id})")]
-        public string GetObservation(int id)
+        public async Task<string> GetObservationAsync(int id)
         {
-            return $"Observation with id: {id}";
+            var service = new ObservationsService(RepoFactory);
+            var observation = await service.GetObservationById(id);
+            observation.BaseUrl = GetBaseUrl();
+
+            return JsonConvert.SerializeObject(observation);
+        }
+
+        [Route(HttpVerbs.Get, "/Observations")]
+        public async Task<string> GetObservationsAsync()
+        {
+            var baseUrl = GetBaseUrl();
+            var service = new ObservationsService(RepoFactory);
+            var observations = await service.GetObservations();
+
+            foreach (var observation in observations)
+            {
+                observation.BaseUrl = baseUrl;
+            }
+
+            var listing = new Listing<Observation>() { Items = observations.ToList() };
+
+            return JsonConvert.SerializeObject(listing);
+        }
+
+        [Route(HttpVerbs.Patch, "/Observations({id})")]
+        public async Task UpdateObservationAsync(int id)
+        {
+            var data = await HttpContext.GetRequestBodyAsStringAsync();
+            var updates = JObject.Parse(data);
+
+            var service = new ObservationsService(RepoFactory);
+            await service.UpdateObservation(updates, id);
+        }
+
+        [Route(HttpVerbs.Delete, "/Observations({id})")]
+        public async Task RemoveObservationAsync(int id)
+        {
+            var service = new ObservationsService(RepoFactory);
+            await service.RemoveObservation(id);
         }
     }
 }
---
+++ SensorThings.Server/Services/ObservationsService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using SensorThings.Entities;
+using SensorThings.Server.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorThings.Server.Services
+{
+    public class ObservationsService
+    {
+        protected IRepositoryFactory RepoFactory { get; private set; }
+
+        public ObservationsService(IRepositoryFactory repoFactory)
+        {
+            RepoFactory = repoFactory;
+        }
+
+        public async Task<Observation> AddObservation(Observation observation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Observation> GetObservationById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Observation>> GetObservations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Observation> UpdateObservation(JObject updates, int id)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public async Task RemoveObservation(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
