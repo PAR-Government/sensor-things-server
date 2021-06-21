0f62d38668cad588bc99828770a40b74ab574423
Gregory Hasseler <gregory_hasseler@partech.com>
2021-06-21 15:50:39 -0500

    Stub in FeaturesOfInterestService

---
+++ SensorThings.Server/Services/FeaturesOfInterestService.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json.Linq;
+using SensorThings.Entities;
+using SensorThings.Server.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorThings.Server.Services
+{
+    public class FeaturesOfInterestService
+    {
+        protected IRepositoryFactory RepoFactory { get; private set; }
+
+        public FeaturesOfInterestService(IRepositoryFactory repoFactory)
+        {
+            RepoFactory = repoFactory;
+        }
+
+        public async Task<FeatureOfInterest> AddFeature(FeatureOfInterest feature)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<FeatureOfInterest> GetFeatureById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<FeatureOfInterest>> GetFeatures()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<FeatureOfInterest> UpdateFeature(JObject updates, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RemoveFeature(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
