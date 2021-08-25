b3ebf7a150ec806b08627025c49e1d9211717900
Gregory Hasseler <gregory_hasseler@partech.com>
2021-08-25 13:53:44 -0500

    Cleaned up example code

--- TestDriver/Driver.cs
+++ TestDriver/Driver.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using Newtonsoft.Json;
 using SensorThings.Server;
-using SensorThings.Entities;
 using System.IO;
 using SensorThings.Server.Repositories;
 using MQTTnet;
@@ -16,33 +14,64 @@ namespace TestDriver
     {
         public static async Task Main()
         {
+            // Create our MQTT pieces
             var factory = new MqttFactory();
             var mqttServer = factory.CreateMqttServer();
             var mqttClient = factory.CreateMqttClient();
 
-            IPHostEntry ipHostInfo = Dns.GetHostEntry("127.0.0.1");
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            // Figure out our listening interfaces
+            //IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
+            //IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPAddress mqttIpAddress = ipAddress;
 
-            Console.WriteLine($"IP Address: {ipAddress}");
+            Console.WriteLine($"Server IP Address: {ipAddress}");
+            Console.WriteLine($"MQTT Broker IP Address: {mqttIpAddress}");
 
-            var mqttServerOptions = new MqttServerOptionsBuilder()
-                .WithDefaultEndpointBoundIPAddress(ipAddress)
-                .WithDefaultEndpointBoundIPV6Address(IPAddress.None)
-                .Build();
+            // Configure MQTT broker. If you are reusing an existing broker, then you don't need
+            // to run this one. Instead just point the client to your existing broker
+            var mqttServerOptionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionValidator(c =>
+                {
+                    c.ReasonCode = MQTTnet.Protocol.MqttConnectReasonCode.Success;
+                });
+
+            // Please not that localhost or loopback may not work on Android per the MQTTnet project
+            if (mqttIpAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+            {
+                mqttServerOptionsBuilder.WithDefaultEndpointBoundIPV6Address(mqttIpAddress);
+            } 
+            else
+            {
+                mqttServerOptionsBuilder.WithDefaultEndpointBoundIPAddress(mqttIpAddress);
+            }
+
+            var mqttServerOptions = mqttServerOptionsBuilder.Build();
+
+            // Start up MQTT broker
             await mqttServer.StartAsync(mqttServerOptions);
 
-            //var mqttServerTask = mqttServer.StartAsync(new MqttServerOptions());
-
+            // Configure MQTT Client. The server will connect it later
             var mqttClientOptions = new MqttClientOptionsBuilder()
-                .WithTcpServer("127.0.0.1")
+                .WithTcpServer(mqttIpAddress.ToString())
                 .Build();
 
+            // Our server will use SQLite for the storage backend
             var dbPath = Path.Combine(Environment.CurrentDirectory, "sensorthings.db");
-            var server = new Server("http://127.0.0.1:8080", new SqliteRepositoryFactory(dbPath), mqttClient, mqttClientOptions);
+            IRepositoryFactory sqlLiteRepoFactory = new SqliteRepositoryFactory(dbPath);
 
+            // Create our server
+            var server = new Server($"http://{ipAddress}:8080", sqlLiteRepoFactory, mqttClient, mqttClientOptions);
+
+            // Init the server and then start it
             server.Configure();
-            var t = server.RunAsync();
+            await server.RunAsync();
+
             Console.ReadLine();
+
+            // Shutdown
+            server.Dispose();
+            await mqttServer.StopAsync();
         }
     }
 }
