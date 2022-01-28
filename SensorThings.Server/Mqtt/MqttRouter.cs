cfae64f480632526c3799aebb4f034bc0c188202
Gregory Hasseler <gregory_hasseler@partech.com>
2022-01-28 12:57:01 -0600

    Created MqttRouter class
    MqttRouter class receives raw MQTT messages and moves them through to the appropriate controller(s) for processing

---
+++ SensorThings.Server/Mqtt/MqttRouter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using MQTTnet;
+using SensorThings.Server.Utils;
+
+namespace SensorThings.Server.Mqtt
+{
+    public class MqttRouter
+    {
+        private const string observationPattern = @"^(?<prefix>v1.0/)?Observations";
+        private readonly Regex observationRegex = new Regex(observationPattern);
+
+        private readonly IObservationsMqttController _observationsController;
+
+        public MqttRouter(IObservationsMqttController observationsController)
+        {
+            _observationsController = observationsController;
+        }
+
+        public bool Route(string topic, byte[] payload)
+        {
+            var isHandled = false;
+
+            var match = observationRegex.Match(topic);
+
+            if (match.Success)
+            {
+                isHandled = true;
+
+                var stringPayload = PayloadUtils.ConvertUTF8BytesToString(payload);
+
+                _observationsController.Create(stringPayload);
+            }
+
+            return isHandled;
+        }
+    }
+}
