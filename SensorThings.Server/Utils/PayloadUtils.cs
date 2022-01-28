dda71168f43273ebcb6693bb280eef6921dc4fe7
Gregory Hasseler <gregory_hasseler@partech.com>
2022-01-28 12:26:50 -0600

    Added new PayloadUtils class
    Contains methods for converting payloads to/from string/bytes

---
+++ SensorThings.Server/Utils/PayloadUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace SensorThings.Server.Utils
+{
+	public class PayloadUtils
+	{
+		public static string ConvertUTF8BytesToString(byte[] payload)
+		{
+			if (payload == null || payload.Length == 0)
+			{
+				return string.Empty;
+			}
+
+			return Encoding.UTF8.GetString(payload, 0, payload.Length);
+		}
+
+		public static byte[] ConvertStringToUTF8Bytes(string payload)
+		{
+			return Encoding.UTF8.GetBytes(payload);
+		}
+	}
+}
